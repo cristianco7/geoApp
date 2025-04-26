e4be330bde8d89d0c90b55da860a231d7c5d180f
Cristian Navarro <canavarroq@ufpso.edu.co>
2025-04-26 11:41:41 -0500

    Feat: Configuración del SDK de Mapbox
    - Se configuró el token de acceso en el archivo "mapbox_access_token.xml" para habilitar el uso del SDK de Mapbox.
    - Se añadió la configuración necesaria para utilizar el MapView de Mapbox en la actividad `MainActivity`.

--- app/src/main/java/com/example/geoapp/MainActivity.kt
+++ app/src/main/java/com/example/geoapp/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.geoapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
+        mapView = findViewById(R.id.mapView)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        mapView = MapView(this);
+        mapView.mapboxMap.setCamera(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(-98.0, 39.5))
+                .pitch(0.0)
+                .zoom(2.0)
+                .bearing(0.0)
+                .build()
+        )
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
