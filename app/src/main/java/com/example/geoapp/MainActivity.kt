f27e241c4589243e677768cd3dadc30e100edf6e
Cristian Navarro <canavarroq@ufpso.edu.co>
2025-04-26 12:43:45 -0500

    Feat: Consumo de API GeoJSON
    - Se realizó el consumo de la API GeoJSON al iniciar la aplicación.

--- app/src/main/java/com/example/geoapp/MainActivity.kt
+++ app/src/main/java/com/example/geoapp/MainActivity.kt
@@ -2,32 +2,42 @@ package com.example.geoapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.mapbox.geojson.Point
-import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.layers.addLayer
+import com.mapbox.maps.extension.style.layers.generated.SymbolLayer
+import com.mapbox.maps.extension.style.sources.addSource
+import com.mapbox.maps.extension.style.sources.generated.GeoJsonSource
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
-        mapView = findViewById(R.id.mapView)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mapView = MapView(this);
-        mapView.mapboxMap.setCamera(
-            CameraOptions.Builder()
-                .center(Point.fromLngLat(-98.0, 39.5))
-                .pitch(0.0)
-                .zoom(2.0)
-                .bearing(0.0)
+
+        mapView = findViewById(R.id.mapView)
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) {
+            loadGeoJson()
+        }
+
+
+    }
+
+    private fun loadGeoJson() {
+        mapView.getMapboxMap().getStyle { style ->
+            val geoJsonSource = GeoJsonSource.Builder("geojson-source")
+                .url("https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_populated_places_simple.geojson")
                 .build()
-        )
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+            style.addSource(geoJsonSource)
+
+            val symbolLayer = SymbolLayer("geojson-layer", "geojson-source")
+            symbolLayer.iconImage("marker-icon")
+            symbolLayer.iconAllowOverlap(true)
+            symbolLayer.iconIgnorePlacement(true)
+
+            style.addLayer(symbolLayer)
         }
     }
 }
