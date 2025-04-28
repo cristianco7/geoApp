a72dbd2cb894523b9e1d9df5f5fafc1690d21368
cristianco7 <canavarroq@ufpso.edu.co>
2025-04-28 16:38:25 -0500

    Feat: Añadir puntos al mapa y carga de iconos personalizados
    - Se implementó la lógica para agregar puntos en el mapa.
    - Se creó un bottom sheet para cambiar el mapa base.
    - Se cargaron los iconos necesarios para el diseño.

--- app/src/main/java/com/example/geoapp/MainActivity.kt
+++ app/src/main/java/com/example/geoapp/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.geoapp
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.geoapp.tools.LocationManager
 import com.example.geoapp.tools.MapManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.gestures.addOnMapClickListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         mapView = findViewById(R.id.mapView)
         val btnCenterLocation = findViewById<ImageButton>(R.id.btnCenterLocation)
+        val btnChangeBaseMap = findViewById<ImageButton>(R.id.btnChangeBaseMap)
         mapManager = MapManager(mapView)
         locationManager = LocationManager(this, mapView, requestPermissionLauncher)
 
@@ -34,6 +40,39 @@ class MainActivity : AppCompatActivity() {
 
         btnCenterLocation.setOnClickListener {
             locationManager.checkAndEnableLocation()
+        }
+
+        mapView.mapboxMap.addOnMapClickListener { point ->
+            mapManager.addPointToMap(point)
+            true
+        }
+
+        btnChangeBaseMap.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(this)
+            val view = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_basemap, null)
+            bottomSheetDialog.setContentView(view)
+
+            val imgStreets = view.findViewById<ImageView>(R.id.imgStreets)
+            val imgSatellite = view.findViewById<ImageView>(R.id.imgSatellite)
+            val imgOutdoors = view.findViewById<ImageView>(R.id.imgOutdoors)
+
+            imgStreets.setOnClickListener {
+                mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS)
+                bottomSheetDialog.dismiss()
+            }
+
+            imgSatellite.setOnClickListener {
+                mapView.mapboxMap.loadStyle(Style.SATELLITE)
+                bottomSheetDialog.dismiss()
+            }
+
+            imgOutdoors.setOnClickListener {
+                mapView.mapboxMap.loadStyle(Style.OUTDOORS)
+                bottomSheetDialog.dismiss()
+            }
+            if(!bottomSheetDialog.isShowing) {
+                bottomSheetDialog.show()
+            }
         }
 
 
--- app/src/main/java/com/example/geoapp/tools/MapManager.kt
+++ app/src/main/java/com/example/geoapp/tools/MapManager.kt
@@ -1,21 +1,37 @@
 package com.example.geoapp.tools
 
+import android.graphics.BitmapFactory
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.toBitmap
+import com.example.geoapp.R
+import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
 import com.mapbox.maps.extension.style.layers.addLayer
 import com.mapbox.maps.extension.style.layers.generated.SymbolLayer
 import com.mapbox.maps.extension.style.sources.addSource
 import com.mapbox.maps.extension.style.sources.generated.GeoJsonSource
+import com.mapbox.maps.extension.style.sources.getSourceAs
 
 class MapManager(private val mapView: MapView) {
+
+    private var isPointAdded = false
 
     companion object {
         private const val GEOJSON_URL =
             "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_populated_places_simple.geojson"
+        private const val SOURCE_ID = "clicked-point-source"
+        private const val LAYER_ID = "clicked-point-layer"
     }
 
     fun initializeMap(onStyleLoaded: () -> Unit) {
-        mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS) {
+        mapView.mapboxMap.loadStyle(Style.MAPBOX_STREETS) { style ->
+            val drawable =
+                AppCompatResources.getDrawable(mapView.context, R.drawable.ic_location_marker_icon)
+            val bitmap = drawable?.toBitmap()
+            if (bitmap != null) {
+                style.addImage("ic_location-marker-icon", bitmap)
+            }
             loadGeoJson()
             onStyleLoaded()
         }
@@ -32,6 +48,43 @@ class MapManager(private val mapView: MapView) {
             symbolLayer.iconImage("marker-icon")
             symbolLayer.iconAllowOverlap(true)
             symbolLayer.iconIgnorePlacement(true)
+            style.addLayer(symbolLayer)
+        }
+    }
+
+
+    fun addPointToMap(point: Point) {
+        mapView.mapboxMap.getStyle { style ->
+            if (isPointAdded) {
+                removeSourceAndLayer(style)
+                isPointAdded = false
+            } else {
+                addOrUpdateSource(style, point)
+                addLayerIfNotExits(style)
+                isPointAdded = true
+            }
+        }
+    }
+
+    private fun removeSourceAndLayer(style: Style) {
+        if (style.styleSourceExists(SOURCE_ID)) style.removeStyleSource(SOURCE_ID)
+        if (style.styleLayerExists(LAYER_ID)) style.removeStyleLayer(LAYER_ID)
+    }
+
+    private fun addOrUpdateSource(style: Style, point: Point) {
+        if (style.styleSourceExists(SOURCE_ID)) {
+            style.getSourceAs<GeoJsonSource>(SOURCE_ID)?.geometry(point)
+        } else {
+            val geoJsonSource = GeoJsonSource.Builder(SOURCE_ID).geometry(point).build()
+            style.addSource(geoJsonSource)
+        }
+    }
+
+    private fun addLayerIfNotExits(style: Style) {
+        if (!style.styleLayerExists(LAYER_ID)) {
+            val symbolLayer = SymbolLayer(LAYER_ID, SOURCE_ID)
+                .iconImage("ic_location-marker-icon")
+                .iconAllowOverlap(true)
             style.addLayer(symbolLayer)
         }
     }
